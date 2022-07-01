baa500853d0c191e9c7f5ecdcd5216588b9a52f8
Alisson7Neres <alisson.neres@hotmail.com>
2022-06-30 23:15:52 -0300

    Criando items part 3

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 
 var app = express();
 app.use(express.static('src'));
+app.use(express.static('src/pages'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
 });
+
+app.get('/endereco', (req, res) => {
+    res.sendFile(__dirname + '/src/pages/endereco.html')
+})
 
 app.listen(3030);
